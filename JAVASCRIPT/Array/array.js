62587354df3b048db60c6d8ae70fcd360d301443
Riga Hoever <riga2ni@gmail.com>
2020-10-21 20:30:46 +0200

    update

---
+++ JAVASCRIPT/Array/array.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+console.log(numbers);
+
+const fruit = ["appel","banaan","peer","aardbei","meloen"];
+console.log(fruit);
+console.log(fruit[0]);
+console.log(fruit[3]);
+console.log();
+Math.random()
